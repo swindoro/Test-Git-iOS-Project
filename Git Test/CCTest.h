919649d5c12b0fbc334f53bff931811323edde74
Subianto Windoro <swindoro@yahoo.com>
2015-01-01 23:28:28 -0800

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Subianto Windoro on 1/1/15.
+//  Copyright (c) 2015 Subianto Windoro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Subianto Windoro on 1/1/15.
+//  Copyright (c) 2015 Subianto Windoro. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
